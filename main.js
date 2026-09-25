09695d180628e086c1dd9d3b957a9d51cf44be0b
emilsopr-sudo <emilsopr@gmail.com>
2026-09-25 05:14:45 +0300

    Set up Express server with basic route

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('הבוט פעיל 24/7!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fs = require('fs').promises;
 const path = require('path');
